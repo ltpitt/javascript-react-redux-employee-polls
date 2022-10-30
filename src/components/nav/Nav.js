7db8150b8ce0b39e1d839fe74d6bd5cca4f41da3
Davide Nastri <d.nastri@gmail.com>
2022-10-30 22:57:04 +0100

    feat: Completed Navbar design

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,25 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
-const Nav = (users) => {
+const Nav = ({ authedUser, users }) => {
   return (
     <div className="container">
-      {/* <!-- Navbar --> */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        {/* <!-- Container wrapper --> */}
         <div className="container-fluid">
-          {/* <!-- Navbar brand --> */}
           <a className="navbar-brand" href="#">
-            <img
-              src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png"
-              height="30"
-              alt=""
-              loading="lazy"
-            />
+            <img src="/logo192.png" height="30" alt="" loading="lazy" />
           </a>
 
-          {/* <!-- Toggle button --> */}
           <button
             className="navbar-toggler"
             type="button"
@@ -32,9 +24,7 @@ const Nav = (users) => {
             <i className="fas fa-bars"></i>
           </button>
 
-          {/* <!-- Collapsible wrapper --> */}
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {/* <!-- Left links --> */}
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link className="nav-link" to="/">
@@ -51,19 +41,16 @@ const Nav = (users) => {
                   New
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/auth">
-                  {users.authedUser
-                    ? "Logout ( " + users.users[users.authedUser].name + " )"
-                    : "Login"}
-                </Link>
-              </li>
             </ul>
-            {/* <!-- Left links --> */}
 
+            {authedUser
+              ? "Hi,  " + users[authedUser].name + "."
+              : "Please login"}
             <ul className="navbar-nav mb-2 mb-lg-0">
-              {/* <!-- Navbar dropdown --> */}
-              <li className="nav-item dropdown">
+              <li
+                className="nav-item dropdown"
+                key={authedUser ? users[authedUser].id : ""}
+              >
                 <a
                   className="nav-link dropdown-toggle"
                   href="#"
@@ -73,76 +60,49 @@ const Nav = (users) => {
                   aria-expanded="false"
                 >
                   <img
-                    src="https://mdbootstrap.com/img/Photos/Avatars/img%20(2).jpg"
+                    src={authedUser ? users[authedUser].avatarURL : ""}
                     className="rounded-circle img-fluid"
                     height="25"
                     width="25"
                   />
                 </a>
-                {/* <!-- Dropdown menu --> */}
+
                 <ul
                   className="dropdown-menu dropdown-menu-end p-1"
                   aria-labelledby="navbarDropdown"
                 >
-                  <li className="my-2 d-flex align-items-center">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(4).jpg"
-                      className="rounded-circle img-fluid me-1"
-                      height="25"
-                      width="25"
-                    />
-                    <span> User 1</span>
-                  </li>
-                  <li className="my-2 d-flex align-items-center">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(6).jpg"
-                      className="rounded-circle img-fluid me-1"
-                      height="25"
-                      width="25"
-                    />
-                    <span> User 2</span>
-                  </li>
-                  <li className="my-2 d-flex align-items-center">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
-                      className="rounded-circle img-fluid me-1"
-                      height="25"
-                      width="25"
-                    />
-                    <span> User 3</span>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Manage Profilses
-                    </a>
-                  </li>
+                  {Object.keys(users)
+                    .filter((user) => user !== users[authedUser].id)
+                    .map((user) => (
+                      <li
+                        key={users[user].id}
+                        className="my-2 d-flex align-items-center"
+                      >
+                        <a className="dropdown-item" href="#">
+                          <img
+                            src={users[user].avatarURL}
+                            className="rounded-circle img-fluid me-1"
+                            height="25"
+                            width="25"
+                          />
+                          <span> {users[user].name}</span>
+                        </a>
+                      </li>
+                    ))}
                   <li>
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
-                      Your Account
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Help
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
+                    <Link className="nav-link" to="/auth">
                       Log Out
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </li>
             </ul>
           </div>
-          {/* <!-- Collapsible wrapper --> */}
         </div>
-        {/* <!-- Container wrapper --> */}
       </nav>
-      {/* <!-- Navbar --> */}
     </div>
   );
 };
